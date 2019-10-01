e9085536f4be8f88954e1099b190ee83eafc4fe3
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2019-10-01 15:26:12 +0200

    Add Creating New Campgrounds route

--- app.js
+++ app.js
@@ -5,33 +5,56 @@ const express = require("express");
 const app = express();
 const port = 80;
 
+var bodyParser = require("body-parser");
+
 app.set("view engine", "ejs");
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+let campgrounds = [
+  {
+    name: "Salmon Creek",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069__340.jpg"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://cdn.pixabay.com/photo/2016/09/18/18/18/tent-camping-1678714__340.jpg"
+  },
+  {
+    name: "Radawa",
+    image:
+      "https://cdn.pixabay.com/photo/2013/09/16/19/15/camp-182951__340.jpg"
+  }
+];
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-  let campgrounds = [
-    {
-      name: "Salmon Creek",
-      image:
-        "https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069__340.jpg"
-    },
-    {
-      name: "Granite Hill",
-      image:
-        "https://cdn.pixabay.com/photo/2016/09/18/18/18/tent-camping-1678714__340.jpg"
-    },
-    {
-      name: "Radawa",
-      image:
-        "https://cdn.pixabay.com/photo/2013/09/16/19/15/camp-182951__340.jpg"
-    }
-  ];
+ 
 
   res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", (req, res) => {
+  //get data from form and add to campgrounds
+  let name  = req.body.name;
+  let image = req.body.image;
+  let newCampground = {name: name, image: image}
+
+  campgrounds.push(newCampground);
+  //redirect back to campgrounds page
+   res.redirect("/campgrounds");
+
+   
+});
+
+app.get("/campgrounds/new", (req, res)=> {
+  res.render("new");
 });
 
 app.listen(port, () => {
