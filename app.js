932254329f56394247e0e664ff202008feac780b
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2019-10-16 22:29:52 +0200

    add create comments route

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 const express = require("express");
 const mongoose = require("mongoose");
-var Campground = require("./models/campground")
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 var bodyParser = require("body-parser");
 var seedDB = require("./seeds");
 
@@ -9,39 +10,38 @@ const app = express();
 const port = 3000;
 
 //set parameteres to remove deprecation message
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
 //connect to local data base
 mongoose.connect("mongodb://localhost:27017/yelp_camp");
 
-
 app.set("view engine", "ejs");
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 seedDB();
 
 // ROUTES
-
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-
   //get all campgrounds from database
 
-  Campground.find({}, function (err, allCampgrounds) {
+  Campground.find({}, function(err, allCampgrounds) {
     if (err) {
       console.log(err);
     } else {
-      res.render("index", {
+      res.render("campgrounds/index", {
         campgrounds: allCampgrounds
       });
     }
@@ -57,7 +57,7 @@ app.post("/campgrounds", (req, res) => {
     name: name,
     image: image,
     description: description
-  }
+  };
 
   //Create a new campground and save to DB
   Campground.create(newCampground, (err, newlyCreated) => {
@@ -71,23 +71,57 @@ app.post("/campgrounds", (req, res) => {
 });
 
 app.get("/campgrounds/new", (req, res) => {
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
 // SHOW ROUTE - wazna jest kolejnosc tej sciezki. musi byc po scieze /campgrounds/new
 app.get("/campgrounds/:id", (req, res) => {
-  let id = req.params.id
-  Campground.findById(id).populate("comments").exec(function (err, foundCampground) {
+  let id = req.params.id;
+  Campground.findById(id)
+    .populate("comments")
+    .exec(function(err, foundCampground) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("campgrounds/show", {
+          campground: foundCampground
+        });
+      }
+    });
+});
+
+// =======================================
+// COMMENTS ROUTES
+// =======================================
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
     if (err) {
       console.log(err);
     } else {
-      res.render("show", {
-        campground: foundCampground
-      });
+      res.render("comments/new", { campground: campground });
     }
   });
 });
 
+app.post("/campgrounds/:id/comments/", (req, res) => {
+  Campground.findById(req.params.id, function(err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  });
+});
 
 // START Server to listen for requests
 app.listen(port, () => {
