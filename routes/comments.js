df2558344f37b446960a0bc44bb93c3c3e5d685b
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2019-10-27 07:32:11 +0100

    add delete comment route

--- routes/comments.js
+++ routes/comments.js
@@ -77,4 +77,16 @@ router.put('/:comment_id', (req, res) => {
   });
 });
 
+// Destroy comment route
+
+router.delete('/:comment_id', (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, err => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
+});
+
 module.exports = router;
